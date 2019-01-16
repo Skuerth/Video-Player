2b62f55e7d1de0ee782735ebfb07703a2f24e765
Skuer <skuerth@gmail.com>
2019-01-16 11:11:09 +0800

    first commit

---
+++ Video Player/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Video Player
+//
+//  Created by Skuerth on 2019/1/16.
+//  Copyright © 2019 Skuerth. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+}
